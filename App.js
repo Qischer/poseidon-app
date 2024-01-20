3fb25303e11514eba927d2110cfd62ee942c44e8
Max Chen <bobandjoe132@gmail.com>
2024-01-20 01:53:24 -0500

    Basic calendar testing

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
 
 export default function App() {
+
+  const [selected, setSelected] = useState('');
+
   return (
+    
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Calendar
+      onDayPress={day => {
+        setSelected(day.dateString);
+      }}
+      markedDates={{
+        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+      }}
+      />
       <StatusBar style="auto" />
     </View>
   );
