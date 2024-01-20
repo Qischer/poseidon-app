8d0ff8165b6f6356abd5c89b0e3e3d76d5e68dd8
Cailey Jen <caileyjen@gmail.com>
2024-01-20 14:52:08 -0500

    global style udate for login and signup, changed app flow to login, added keyboard dismiss
    Signed-off-by: Cailey Jen <caileyjen@gmail.com>

--- app/index.js
+++ app/index.js
@@ -1,36 +1,80 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { SafeAreaView, Text, View, TextInput, TouchableOpacity, StatusBar, Alert, LogBox, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { auth } from "../services/firebase";
+import { router } from "expo-router";
+import { globalStyles } from "../global";
 
-const WelcomeScreen = ({ navigation }) => {
+LogBox.ignoreAllLogs();
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onHandleLogin = () => {
+    if (email !== "" && password !== "") {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(() => console.log("Login success")).then(() => router.push("/home"))
+        .catch((err) => Alert.alert("Login error", err.message));
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome you!</Text>
-
-      <View style={styles.buttons}>
-        <Link href="/signin" style={styles.button}>Sign In</Link>
-        <Link href="/signup" style={styles.button}>Sign Up</Link>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={globalStyles.container}>
+    
+        {/* Title */}
+        <Text style={globalStyles.title}> Log In</Text>
+    
+        <SafeAreaView>
+          {/* Input Fields */}
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Enter email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            autoFocus={false}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Enter password"
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry={true}
+            textContentType="password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+    
+          {/* Login Button */}
+          <TouchableOpacity onPress={onHandleLogin}>
+            <View style={globalStyles.button}>
+              <Text style={globalStyles.buttonText}>Log In</Text>
+            </View>
+          </TouchableOpacity>
+    
+          {/* Navigation to Signup Screen */}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              alignSelf: "center",
+            }}
+          >
+            <Text style={globalStyles.impactText}>
+              Don't have an account?{" "}
+            </Text>
+            <TouchableOpacity onPress={() => router.push("/signup")}>
+              <Text style={globalStyles.link}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+        {/* StatusBar */}
+        <StatusBar barStyle="light-content" />
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 90,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  buttons: {
-    flex: 1,
-  },
-
-  button: {
-    marginTop: 40
-  }
-});
-
-export default WelcomeScreen;
--- app/signup.js
+++ app/signup.js
@@ -1,9 +1,10 @@
-import { SafeAreaView, Text, View, TextInput, TouchableOpacity, StatusBar, Alert, StyleSheet } from "react-native";
+import { SafeAreaView, Text, View, TextInput, TouchableOpacity, StatusBar, Alert, StyleSheet, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../services/firebase";
 import { setDoc, doc } from "firebase/firestore";
 import { router } from "expo-router";
+import { globalStyles } from "../global";
 
 export default function SignUp() {
   const [name, setName] = useState("");
@@ -32,70 +33,73 @@ export default function SignUp() {
   }
 
   return (
-  <View style={styles.container}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={globalStyles.container}>
 
-    {/* Title */}
-    <Text>Sign Up</Text>
+        {/* Title */}
+        <Text style={globalStyles.title}>Sign Up</Text>
 
-    <SafeAreaView>
-      {/* Input Fields */}
-      <TextInput
-        // style={styles.input}
-        placeholder="Enter name"
-        autoCapitalize="none"
-        autoCorrect={false}
-        textContentType="name"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <TextInput
-        // style={styles.input}
-        placeholder="Enter email"
-        autoCapitalize="none"
-        keyboardType="email-address"
-        textContentType="emailAddress"
-        autoFocus={true}
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        // style={styles.input}
-        placeholder="Enter password"
-        autoCapitalize="none"
-        autoCorrect={false}
-        showSoftInputOnFocus={true}
-        secureTextEntry={true}
-        textContentType="password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
+        <SafeAreaView>
+          {/* Input Fields */}
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Enter name"
+            autoCapitalize="none"
+            autoCorrect={false}
+            textContentType="name"
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Enter email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            autoFocus={false}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <TextInput
+            style={globalStyles.input}
+            placeholder="Enter password"
+            autoCapitalize="none"
+            autoCorrect={false}
+            showSoftInputOnFocus={true}
+            secureTextEntry={true}
+            textContentType="password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
 
-      {/* Signup Button */}
-      <TouchableOpacity onPress={onHandleSignup}>
-        <Text style={{ fontWeight: "bold", color: "gray", fontSize: 18 }}>Sign Up</Text>
-      </TouchableOpacity>
+          {/* Signup Button */}
+          <TouchableOpacity onPress={onHandleSignup}>
+            <View style={globalStyles.button}>
+              <Text style={globalStyles.buttonText}>Sign Up</Text>
+            </View>
+          </TouchableOpacity>
 
-      {/* Navigation to Login Screen */}
-      <View
-        style={{
-          marginTop: 20,
-          flexDirection: "row",
-          alignItems: "center",
-          alignSelf: "center",
-        }}
-      >
-        <Text style={{ color: "gray", fontWeight: "600", fontSize: 14 }}>
-          Don't have an account?{" "}
-        </Text>
-        <TouchableOpacity onPress={() => router.push("/signin")}>
-          <Text style={{ color: "#f57c00", fontWeight: "600", fontSize: 14 }}>Log In</Text>
-        </TouchableOpacity>
+          {/* Navigation to Login Screen */}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              alignSelf: "center",
+            }}
+          >
+            <Text style={globalStyles.impactText}>
+              Don't have an account?{" "}
+            </Text>
+            <TouchableOpacity onPress={() => router.push("/")}>
+              <Text style={globalStyles.link}>Log In</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+        {/* StatusBar */}
+        <StatusBar barStyle="light-content" />
       </View>
-    </SafeAreaView>
-    {/* StatusBar */}
-    <StatusBar barStyle="light-content" />
-  </View>
-);
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
