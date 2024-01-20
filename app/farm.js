20054c6fd2ca51602985d1cc71e02e620e4357fd
Max Chen <bobandjoe132@gmail.com>
2024-01-20 03:56:58 -0500

    corn

--- app/farm.js
+++ app/farm.js
@@ -1,10 +1,12 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import NavBar from "../components/navbar";
 
 export default function FarmPage() {
     return <View style={{flex: 1}}>
         <ScrollView>
-            <Text>Farm Page</Text>
+            {/* <Text>Farm Page</Text> */}
+            <Image source={require('../assets/corn.png')} />
+            
         </ScrollView>
         <NavBar/>
     </View>
