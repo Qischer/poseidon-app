33398e281369da3deed39c7bd0d20a16bd0e5dd6
Cailey Jen <caileyjen@gmail.com>
2024-01-20 09:33:53 -0500

    timetable changed to another package so now it works but looks scuffed
    Signed-off-by: Cailey Jen <caileyjen@gmail.com>

--- components/calendar.js
+++ components/calendar.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
-
-import { Calendar, Timeline, Agenda } from "react-native-calendars";
+import { Calendar, Timeline } from "react-native-calendars";
+import moment from "moment";
+import Timetable from "react-native-calendar-timetable";
 
 export default function CalendarView() {
-    const [date, setDate] = useState("")
     const [datecolor,setDateColor]=useState("")
 
     const addZero = (a) => {
@@ -20,6 +20,22 @@ export default function CalendarView() {
         var year = new Date().getFullYear();
         return year + '-' + addZero(month) + '-' + addZero(date);//yyyy-mm-dd
     }
+
+    
+    const [date, setDate] = useState(getCurrentDate().toString())
+
+    const [timetableDate] = React.useState(new Date());
+    const [from] = React.useState(moment().subtract(3, 'days').toDate());
+    const [till] = React.useState(moment().add(3, 'days').toISOString());
+    const range = {from, till};
+
+    const [items] = React.useState([
+        {
+            title: 'Some event',
+            startDate: moment().subtract(1, 'hour').toDate(),
+            endDate: moment().add(1, 'hour').toDate(),
+        },
+    ]);
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -45,8 +61,30 @@ export default function CalendarView() {
                     }}
                 />
                 <Text style={{fontSize:20,textAlign:'center',fontSize:25,fontWeight:'bold'}}>{date}</Text>
-                <Timeline/>
+                <Timetable
+                // these two are required
+                items={items}
+                renderItem={props => <YourComponent {...props}/>}
+
+                // provide only one of these
+                date={timetableDate}
+                range={range}
+            />
             </View>
         </SafeAreaView>
     )
+}
+
+export function YourComponent({style, item, dayIndex, daysTotal}) {
+    return (
+        <View style={{
+            ...style, // apply calculated styles, be careful not to override these accidentally (unless you know what you are doing)
+            backgroundColor: 'red',
+            borderRadius: 10,
+            elevation: 5,
+        }}>
+            <Text>{item.title}</Text>
+            <Text>{dayIndex} of {daysTotal}</Text>
+        </View>
+    );
 }
