78d74cb6d90fd2bf4e132b905372dfa38f02af3f
Cailey Jen <caileyjen@gmail.com>
2024-01-21 03:17:53 -0500

    i cant merge
    Signed-off-by: Cailey Jen <caileyjen@gmail.com>

--- app/settings.js
+++ app/settings.js
@@ -4,24 +4,32 @@ import { Avatar } from "@rneui/themed";
 import { UserAuth } from "../services/authContext";
 import { globalStyles } from "../global";
 import { Feather } from '@expo/vector-icons';
+import { router } from "expo-router";
 
 export default function SettingsPage() {
-    const {user} = UserAuth();
+    const { user, logOut } = UserAuth();
+
+    const handleLogout = () => {
+        console.log("log out");
+        logOut();
+
+        setTimeout(() => router.push('/'), 1000);
+    }
+
     return <View style={{ flex: 1}}>
         <ScrollView>
-            
-        <View style={{ alignItems: 'center', paddingTop: 100}}>
-            <Avatar
-                containerStyle={{ backgroundColor: "#BDBDBD" }}
-                icon={{name: 'user', type: 'font-awesome'}}
-                rounded
-                size={100}
-            />
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <Text style={{marginVertical: 15, marginHorizontal: 5}}>{user?.email}</Text>
-                <Feather name='edit-2' color={'black'} size={15} />
+            <View style={{ alignItems: 'center', paddingTop: 100}}>
+                <Avatar
+                    containerStyle={{ backgroundColor: "#BDBDBD" }}
+                    icon={{name: 'user', type: 'font-awesome'}}
+                    rounded
+                    size={100}
+                />
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={{marginVertical: 15, marginHorizontal: 5}}>{user?.email}</Text>
+                    <Feather name='edit-2' color={'black'} size={15} />
+                </View>
             </View>
-        </View>
         </ScrollView>
 
         <Pressable onPress={handleLogout}>
