997e076d2d242f7db6b28a1444953ee8c4d13cb0
Cailey Jen <caileyjen@gmail.com>
2024-01-21 02:20:16 -0500

    settings work
    Signed-off-by: Cailey Jen <caileyjen@gmail.com>

--- app/settings.js
+++ app/settings.js
@@ -1,10 +1,27 @@
 import { View, Text, ScrollView } from "react-native";
 import NavBar from "../components/navbar";
+import { Avatar } from "@rneui/themed";
+import { UserAuth } from "../services/authContext";
+import { globalStyles } from "../global";
+import { Feather } from '@expo/vector-icons';
 
 export default function SettingsPage() {
-    return <View style={{flex: 1}}>
+    const {user} = UserAuth();
+    return <View style={{ flex: 1}}>
         <ScrollView>
-            <Text>Settings Page</Text>
+            
+        <View style={{ alignItems: 'center', paddingTop: 100}}>
+            <Avatar
+                containerStyle={{ backgroundColor: "#BDBDBD" }}
+                icon={{name: 'user', type: 'font-awesome'}}
+                rounded
+                size={100}
+            />
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Text style={{marginVertical: 15, marginHorizontal: 5}}>{user?.email}</Text>
+                <Feather name='edit-2' color={'black'} size={15} />
+            </View>
+        </View>
         </ScrollView>
         <NavBar/>
     </View>
